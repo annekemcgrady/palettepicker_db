7229c944162358cfb81ed7a9ded9f812a1beacf3
Anneke McGrady <annekemcgrady@gmail.com>
2019-08-20 16:30:03 -0600

    Create sample data file and seed file

---
+++ db/data.js
@@ -0,0 +1,20 @@
+export const projectsData =  [
+  {name: 'Kitchen',
+    palettes: [
+  {name: 'Seabreeze', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'},
+  {name: 'Red Baron', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'},
+  {name: 'Panda', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'}]
+  },
+  {name: 'Master Bath',
+  palettes: [
+{name: 'Seabreeze', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'},
+{name: 'Red Baron', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'},
+{name: 'Panda', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'}]
+},
+{name: 'Rumpus Room',
+palettes: [
+{name: 'Seabreeze', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'},
+{name: 'Red Baron', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'},
+{name: 'Panda', color_one: '#bfe9d4', color_two:'#5f9ee5', color_three: '#f28e98', color_four:'#4740b7', color_five: '#7939da'}]
+}
+]
